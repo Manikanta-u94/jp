25cd137fb661ae3c51ab7b850d2e6ff9aecd3bdc
Manikanta-u94 <46936521+Manikanta-u94@users.noreply.github.com>
2019-03-12 18:41:20 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobportal',
+        'USER': 'jobportal',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
